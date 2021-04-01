8fa7a957edd76c92ca9d9ffdfdcce62e22be771f
Hunter <HunterAmin97@gmail.com>
2021-04-01 04:39:44 -0400

    Changed items

--- src/main/java/ShopApp.java
+++ src/main/java/ShopApp.java
@@ -49,39 +49,39 @@ public class ShopApp {
 
 
     public static void main(String[] args) {
-        Clothing shirt = new Clothing("Blue Shirt", 14.99, 'M');
-        Clothing pants = new Clothing("Dress Pants", 34.99, 'L');
-        Clothing shirt2 = new Clothing("Red Shirt", 19.99, 'S');
+        Clothing jeans = new Clothing("Blue Jeans", 20, 'M');
+        Clothing tShirt = new Clothing("White T Shirt", 5.0,'S');
+        Clothing buttonUp = new Clothing("Button Up Shirt", 30.0, 'L');
 
-        Clothing[] managerCart = {shirt, pants, shirt2};
-        Employee m1 = new Manager(managerCart, 'M', "Cashier Joe");
+        Clothing[] managerCart = {jeans, tShirt, buttonUp};
+        Employee m1 = new Manager(managerCart, 'M', "Jim Jones");
 
-        Clothing[] employeeCart = {shirt, pants, shirt2};
-        Employee e1 = new HourlyEmployee(employeeCart, 'L', "Boss Bob");
+        Clothing[] employeeCart = {jeans, tShirt, buttonUp};
+        Employee e1 = new HourlyEmployee(employeeCart, 'L', "Mike Jones");
         System.out.println("Total of Manager's Cart-");
         System.out.println("$" + calcTotal(managerCart));
 
         System.out.println("\nCheck if a clothing fits an employee-");
-        System.out.println(isAFit(e1, pants));
+        System.out.println(isAFit(m1, tShirt));
 
         System.out.println("\nHourly Employee price after discount-");
-        e1.printEmpPriceAfterDisc(pants);
-        e1.printEmpPriceAfterDisc(shirt);
+        System.out.println("Jeans");
+        e1.printEmpPriceAfterDisc(jeans);
+        System.out.println("T-Shirt");
+        e1.printEmpPriceAfterDisc(tShirt);
 
         System.out.println("\nManager price after discount-");
-        m1.printEmpPriceAfterDisc(pants);
-        m1.printEmpPriceAfterDisc(shirt);
+        System.out.println("Jeans");
+        m1.printEmpPriceAfterDisc(jeans);
+        System.out.println("T-Shirt");
+        m1.printEmpPriceAfterDisc(tShirt);
 
 
         ArrayList<Clothing> clothes = new ArrayList<Clothing>();
-        clothes.add(shirt);
-        clothes.add(pants);
-        clothes.add(shirt2);
+        clothes.add(jeans);
+        clothes.add(buttonUp);
+        clothes.add(tShirt);
 
-
-        for(Clothing c : clothes){
-            System.out.println(c.getDescription() + " is size " + c.getSize() + " and costs " + String.format("%s" + "%.2f", "$", c.getPrice()));
-        }
 
         System.out.println("\nPrinting each type of Employee's name-");
         printEmployeeName(m1);
@@ -96,7 +96,7 @@ public class ShopApp {
         sortAndPrintClothingByPrice(clothes);
 
         System.out.println("\nDiscount for type of clothing for each employee-");
-        printDiscAmtOff(pants);
+        printDiscAmtOff(jeans);
 
 
     }
